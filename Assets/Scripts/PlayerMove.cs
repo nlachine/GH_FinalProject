93cc4b995a516eef8d78c632b147a53fc6c4f50f
Noah Lachine <noah.lachine@ontariotechu.net>
2020-12-01 11:54:17 -0500

    [ADD/MOD] Modified movement w/ gravity, added assets and impact effects.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,14 +8,31 @@ public class PlayerMove : MonoBehaviour
 
     public float speed = 12f;
 
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        Debug.Log(isGrounded);
+        if (isGrounded && velocity.y < 0)
+        {
+            Debug.Log("On Ground");
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z; //Calculate updates move vector
 
         controller.Move(move * speed * Time.deltaTime); //Move Player horizontal and vertical
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/gun.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float firerate = 0.01f;
+
+    public int maxAmmo = 30;
+    private int currentAmmo = -1;
+    public float reloadTime = 1f;
+    private float nextFireTime = 0f;
+
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public Text txtAmmoCount;
+
+
+
+    void Start()
+    {
+        nextFireTime = nextFireTime = 1f / firerate;
+        muzzleFlash = gameObject.GetComponentInChildren<ParticleSystem>();
+        fpsCam = gameObject.GetComponentInParent<Camera>();
+        if (currentAmmo == -1)
+            currentAmmo = maxAmmo;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        txtAmmoCount.text = currentAmmo.ToString();
+        if (currentAmmo <= 0 || Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+        if (Input.GetButton("Fire1") && (Time.time >= nextFireTime))
+        {
+            nextFireTime = Time.time + (1f / firerate);
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        currentAmmo--;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitInfo, range))
+        {
+            target Target = hitInfo.transform.GetComponent<target>();
+            if (Target != null)
+            {
+                Target.TakeDamage(damage);
+            }
+
+            GameObject impactParticle = Instantiate( impactEffect, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+            Destroy(impactParticle, 1f);
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        Debug.Log("Reloading");
+        txtAmmoCount.text = "Reloading...";
+        yield return new WaitForSeconds(reloadTime);
+
+        Debug.Log("Reload Complete");
+        currentAmmo = maxAmmo;
+    }
+}
